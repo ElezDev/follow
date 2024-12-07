2cec99f3ccae737260c4a509511a98831b89a2da
ElezDev <edwinledezma2013@gmail.com>
2024-12-07 16:07:12 -0500

    add fix excel

--- app/Exports/AprendizExport.php
+++ app/Exports/AprendizExport.php
@@ -1,10 +1,13 @@
 <?php
-// app/Exports/AprendizExport.php
+
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithTitle;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class AprendizExport implements FromArray
+class AprendizExport implements FromArray, WithStyles, WithTitle
 {
     protected $data;
 
@@ -15,6 +18,58 @@ class AprendizExport implements FromArray
 
     public function array(): array
     {
-        return $this->data;
+        // Encabezado de las columnas
+        $exportData = [
+            [
+                'Nombre Aprendiz',
+                'Identificación Aprendiz',
+                'Departamento Aprendiz',
+                'Municipio Aprendiz',
+                'Ficha Aprendiz',        
+                'Programa Aprendiz',      
+                'Identificación Trainer',
+                'Nombre Trainer',
+                'Departamento Trainer',
+                'Municipio Trainer'
+            ]
+        ];
+
+        foreach ($this->data as $aprendiz) {
+            $exportData[] = [
+                'Nombre Aprendiz' => $aprendiz['name'] . ' ' . $aprendiz['last_name'] ?? 'N/A',
+                'Identificación Aprendiz' => $aprendiz['identification'] ?? 'N/A',
+                'Departamento Aprendiz' => $aprendiz['department'] ?? 'N/A',
+                'Municipio Aprendiz' => $aprendiz['municipality'] ?? 'N/A',
+                'Ficha Aprendiz' => $aprendiz['apprentice']['ficha'] ?? 'N/A', 
+                'Programa Aprendiz' => $aprendiz['apprentice']['programa'] ?? 'N/A',
+                'Identificación Trainer' => $aprendiz['apprentice']['trainer']['user']['identification'] ?? 'N/A',
+                'Nombre Trainer' => ($aprendiz['apprentice']['trainer']['user']['name'] ?? 'N/A') . ' ' . ($aprendiz['apprentice']['trainer']['user']['last_name'] ?? 'N/A'),
+                'Departamento Trainer' => $aprendiz['apprentice']['trainer']['user']['department'] ?? 'N/A',
+                'Municipio Trainer' => $aprendiz['apprentice']['trainer']['user']['municipality'] ?? 'N/A',
+            ];
+        }
+
+        return $exportData;
+    }
+
+    // Establece el título de la hoja
+    public function title(): string
+    {
+        return 'Aprendices';
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        $sheet->getStyle('A1:J1')->getFont()->setBold(true);
+        $sheet->getStyle('A1:J1')->getAlignment()->setHorizontal('center');
+
+        foreach (range('A', 'J') as $columnID) {
+            $sheet->getColumnDimension($columnID)->setAutoSize(true);
+        }
+        
+        $sheet->getRowDimension(1)->setRowHeight(20);
+
+        return [
+        ];
     }
 }
