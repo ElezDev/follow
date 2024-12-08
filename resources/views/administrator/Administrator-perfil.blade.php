cdd6236612b9eefd862c06ead8039f4711204176
Andres Pizo <pizoluligoa@gmail.com>
2024-12-07 19:29:10 -0500

    trying fixing home blade

--- resources/views/administrator/Administrator-perfil.blade.php
+++ resources/views/administrator/Administrator-perfil.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,16 +13,19 @@
         .script-font {
             font-family: 'Dancing Script', cursive;
         }
+
         #userMenu {
             top: 100%;
             margin-top: 0.5rem;
         }
+
         .notifications {
             display: block;
             width: 54px;
             height: auto;
             filter: invert(1);
         }
+
         .Flecha {
             display: block;
             position: absolute;
@@ -30,6 +34,7 @@
             margin-left: 10px;
             margin-top: 40px;
         }
+
         .text-ventana {
             color: #ffffff;
             font-size: 20px;
@@ -42,115 +47,35 @@
         }
     </style>
 </head>
+
 <body class="font-['Arial',sans-serif] bg-white m-0 flex flex-col min-h-screen">
-    <header class="bg-white text-[#009e00] px-5 py-2.5 flex flex-col items-center border-t-[5px] border-t-white border-b border-b-[#e0e0e0]">
-        <div class="flex justify-between w-full">
-            <div class="flex items-center">
-                <img class="w-[70px] h-[70px]" src="{{ asset('img/logo-sena.png') }}" alt="Sena Logo ">
-                <div class="flex-grow m-2"></div>
-                <div class="text-left">
-                    <a href="{{ route('administrator.home') }}" class="flex items-center">
-                        <img src="{{ asset('img/logo.png') }}" alt="Etapa Seguimiento Logo" class="w-10 h-auto mr-1.5">
-                        <div class="flex flex-col text-left">
-                            <h2 class="text-[12px] m-0 text-[#009e00]">Etapa</h2>
-                            <h2 class="text-[12px] m-0 text-[#009e00]">Productiva</h2>
-                        </div>
-                    </a>
-                    <h2 class="text-sm mt-2 text-[#009e00]">Centro de Comercio y Servicios</h2>
-                </div>
-            </div>
-            <div class="relative ml-auto flex items-center">
-                <!-- Contenedor para la imagen y el ícono de los tres puntos -->
-                <div class="relative">
-                    <!-- Imagen de usuario -->
-                    <img class="bg-white w-[45px] h-auto rounded-full border-2 " src="{{ asset('img/administrador/mujer.png') }}" alt="User Icon">
 
-                    <!-- Botón de los tres puntos encima de la imagen -->
-                    <button id="menuButton" class="absolute top-1 right-0 bg-transparent p-1 mr-[-46%]">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="w-8 h-8">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm0 5.25a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm0 5.25a1.5 1.5 0 110-3 1.5 1.5 0 010 3z" />
-                        </svg>
-                    </button>
-                </div>
-                 {{-- Menu --}}
-                 <div id="userMenu" class=" hidden absolute right-4  mt-2 w-64 bg-[#D9D9D9] border border-gray-300 rounded-lg shadow-lg z-20">
-                     <div class="p-4">
-                         <div class="flex items-center mb-4">
-                             <div>
-                                 <p class="text-sm font-bold">{{ auth()->user()->name }} {{ auth()->user()->last_name }}</p>
-                                 <p class="text-sm mt-2">Administrador</p>
-                             </div>
+    @include('partials.header')
 
-
-                         </div>
-                         <ul>
-                             <li class="mt-2"><a href="{{ route('administrator.Administrator-perfil') }}" class="block text-center text-green-600 font-bold bg-white border hover:text-white hover:bg-green-600 border-green-600 rounded-lg py-1">Ver perfil</a></li>
-
-                             <li class="mt-2"><a href="{{ route('administrator.settings') }}" class="block text-black hover:bg-white p-2 rounded-lg">Configuración</a></li>
-
-
-                             <li class="mt-2"><a href="{{ route('administrator.template') }}" class="block text-black hover:bg-white p-2 rounded-lg" onclick="toggleSublist(event)">Plantillas</a>
-                                 <ul class="hidden ml-4 mt-2 bg-[#EEEEEE] p-2 rounded-lg">
-                                     <li class="mt-2 font-bold text-black border-b border-gray-300 pb-2">MODALIDAD</li>
-                                     <li class="mt-2"><a href="{{ route('administrator.template')}}"class="block text-black hover:bg-white p-2 rounded-lg">Pasantía</a></li>
-                                     <a href="javascript:void(0);" class="block text-black hover:bg-white p-2 rounded-lg" onclick="toggleSublist(event)">Contrato de Aprendizaje</a>
-                                     <ul class="hidden ml-4 mt-2 bg-[#D9D9D9] p-2 rounded-lg w-[250px]">
-                                         <li class="mt-2">
-                                             <a href="{{ route('administrator.template') }}" class="block text-black hover:bg-white p-2 rounded-lg">Ver Plantilla</a>
-                                         </li>
-                                         <ul>
-                                             <li class="mt-2">
-                                                 <button id="uploadButton" class="block text-black hover:bg-white p-2 rounded-lg">+ Añadir Plantilla</button>
-                                                 <input type="file" id="fileUpload" class="hidden" name="fileUpload" accept=".txt,.doc" />
-                                             </li>
-                                         </ul>
-                                     </ul>
-                                 </li>
-                                     <li class="mt-2"><a href="{{ route('administrator.template')}}" class="block text-black hover:bg-white p-2 rounded-lg">Vinculo Laboral</a></li>
-                                     <li><a href="{{ route('administrator.template')}}" class="block text-black hover:bg-white p-2 rounded-lg">Unidad Productiva Familiar</a></li>
-                                     <li><a href="{{ route('administrator.template')}}" class="block text-black hover:bg-white p-2 rounded-lg">Proyecto Productivo Empresarial</a></li>
-                                 </ul></li>
-                             <li class="mt-2"><a href="{{ route('administrator.graphic')}}" class="block text-black hover:bg-white p-2 rounded-lg">Graficas</a></li>
-                             <a href="javascript:void(0);" class="block text-black hover:bg-white p-2 rounded-lg" onclick="toggleSublist(event)">Registrar </a>
-          <ul class="hidden ml-4 mt-2 bg-[#D9D9D9] p-2 rounded-lg w-[250px]">
-              <li class="mt-2">
-                  <a href="{{ route('register') }}" class="block text-black hover:bg-white p-2 rounded-lg">Instructor</a>
-              </li>
-                  <li class="mt-2">
-                      <a href="{{ route('register') }}" class="block text-black hover:bg-white p-2 rounded-lg">Aprendiz</a>
-                  </li>
-              </ul>
-
-      </li>
-
-                         </ul>
-                         <form id="logoutForm" action="{{ route('logout') }}" method="POST" class="mt-4">
-                             @csrf
-                             <button type="submit" class="block text-center text-green-600 font-bold bg-white border hover:text-white hover:bg-green-600 border-green-600 rounded-lg py-2 w-full">Cerrar sesión</button>
-                         </form>
-                 </div>
-             </div>
-        </header>
     <nav class="bg-[#009e00] px-2.5 h-14 py-1.5 flex items-center relative z-10">
         <div class="w-full flex justify-center">
             <ul class="horizontal-list flex space-x-4 justify-center">
                 <li>
-                    <a href="{{ route('administrator.home') }}" class="block text-white text-center bg-transparent px-4 py-2 rounded-lg hover:bg-green-700 transition">
+                    <a href="{{ route('administrator.home') }}"
+                        class="block text-white text-center bg-transparent px-4 py-2 rounded-lg hover:bg-green-700 transition">
                         Inicio
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('administrator.apprentice') }}" class="block text-white text-center bg-transparent px-4 py-2 rounded-lg hover:bg-green-700 transition">
+                    <a href="{{ route('administrator.apprentice') }}"
+                        class="block text-white text-center bg-transparent px-4 py-2 rounded-lg hover:bg-green-700 transition">
                         Aprendices
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('administrator.instructor') }}" class="block text-white text-center bg-transparent px-4 py-2 rounded-lg hover:bg-green-700 transition">
+                    <a href="{{ route('administrator.instructor') }}"
+                        class="block text-white text-center bg-transparent px-4 py-2 rounded-lg hover:bg-green-700 transition">
                         Instructores
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('administrator.graphic') }}" class="block text-white text-center bg-transparent px-4 py-2 rounded-lg hover:bg-green-700 transition">
+                    <a href="{{ route('administrator.graphic') }}"
+                        class="block text-white text-center bg-transparent px-4 py-2 rounded-lg hover:bg-green-700 transition">
                         Gráficas
                     </a>
                 </li>
@@ -158,7 +83,8 @@
         </div>
         <button id="notifButton" class="absolute right-0 mr-4">
             <a href="{{ route('administrator.notificaciones') }}">
-                <img class="w-[50px] h-auto filter invert" src="{{ asset('img/notificaciones.png') }}" alt="Notificaciones">
+                <img class="w-[50px] h-auto filter invert" src="{{ asset('img/notificaciones.png') }}"
+                    alt="Notificaciones">
             </a>
         </button>
     </nav>
@@ -172,8 +98,10 @@
                     <img src="{{ asset('img/administrador/mujer.png') }}" alt="User" class="w-40 h-40 z-10">
                     <div class="ml-6 flex flex-col items-start">
                         <div class="flex items-baseline gap-2">
-                            <h1 class="script-font text-4xl font-bold text-black mb-1" style="font-family: 'Times New Roman', serif;">{{ auth()->user()->name }}</h1>
-                            <span class="text-2xl font-light uppercase tracking-wider">{{ auth()->user()->last_name }}</span>
+                            <h1 class="script-font text-4xl font-bold text-black mb-1"
+                                style="font-family: 'Times New Roman', serif;">{{ auth()->user()->name }}</h1>
+                            <span
+                                class="text-2xl font-light uppercase tracking-wider">{{ auth()->user()->last_name }}</span>
                         </div>
                         <p class="text-lg text-black font-light tracking-wide">ADMINISTRADOR</p>
                     </div>
@@ -199,8 +127,10 @@
                 </div>
 
                 <div class="flex justify-end mt-6 space-x-4">
-                    <a href="{{ route('administrator.Actualizar-perfil') }}" class="bg-green-700 hover:bg-green-900 text-white py-2 px-4 rounded">Actualizar</a>
-                    <a href="{{ route('administrator.home') }}" class="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded">Cancelar</a>
+                    <a href="{{ route('administrator.Actualizar-perfil') }}"
+                        class="bg-green-700 hover:bg-green-900 text-white py-2 px-4 rounded">Actualizar</a>
+                    <a href="{{ route('administrator.home') }}"
+                        class="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded">Cancelar</a>
                 </div>
             </div>
         </main>
@@ -208,5 +138,5 @@
     <script src="{{ asset('js/Administrator.js') }}"></script>
 
 </body>
-</html>
 
+</html>
